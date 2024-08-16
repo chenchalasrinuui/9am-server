f1339b159127fc5aaf1c593fa1c4d73c91213049
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2024-08-16 10:46:11 +0530

    token implemetation

--- routes/students.js
+++ routes/students.js
@@ -2,34 +2,42 @@ var express = require('express')
 var mongodb = require('mongodb')
 var objectId = mongodb.ObjectId;
 var router = express.Router();
+var jwt = require('jsonwebtoken')
 var getDB = require('../common/dbConn')
+var validateToken = require('../common/validateToken')
 
-router.post("/register", async function (req, res, next) {// req received
-    try {
-        const data = req.body.data;
-        const db = await getDB();
-        const collection = db.collection('students')
-        const result = await collection.insertOne(data)
-        res.send(result)
+router.post(
+    "/register",
+    validateToken,
+    async function (req, res, next) {// req received
+        try {
+            const data = req.body.data;
+            const db = await getDB();
+            const collection = db.collection('students')
+            const result = await collection.insertOne(data)
+            res.send(result)
 
-    } catch (ex) {
-        res.send(ex.message);
-    }
-})
+        } catch (ex) {
+            res.send(ex.message);
+        }
+    })
 
-router.get('/get-std', async function (req, res, next) {
-    try {
-        const db = await getDB();
-        const collection = db.collection('students');
-        const result = await collection.find().toArray()
-        res.send(result);
+router.get(
+    '/get-std',
+    validateToken,
+    async function (req, res, next) {
+        try {
+            const db = await getDB();
+            const collection = db.collection('students');
+            const result = await collection.find().toArray()
+            res.send(result);
 
-    } catch (ex) {
-        res.send(ex.message);
-    }
-})
+        } catch (ex) {
+            res.send(ex.message);
+        }
+    })
 
-router.put('/update-std', async function (req, res, next) {
+router.put('/update-std', validateToken, async function (req, res, next) {
     try {
         const id = req.query.id;
         const data = req.body.data;
@@ -43,7 +51,7 @@ router.put('/update-std', async function (req, res, next) {
     }
 })
 
-router.delete('/delete-std/:id', async function (req, res, next) {
+router.delete('/delete-std/:id', validateToken, async function (req, res, next) {
     try {
         var id = req.params.id
         const db = await getDB();
@@ -59,7 +67,8 @@ router.delete('/delete-std/:id', async function (req, res, next) {
 router.post("/login", function (req, res, next) {
     const { uid, pwd } = req.body
     if (uid === 'nit' && pwd === 'nitnit') {
-        res.send([{ uid, pwd }])
+        const token = jwt.sign({ uid, pwd }, "appToken")
+        res.send([{ uid, pwd, token }])
     } else {
         res.send([])
     }
